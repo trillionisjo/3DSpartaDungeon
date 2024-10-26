115565b0d15c4d27c3383416110d0c9874b7a98d
trillionisjo <trillionisjo@gmail.com>
2024-10-26 14:02:06 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+}
